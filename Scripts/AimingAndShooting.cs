125f50aabfa6b25b5d1a86a23075f423b895828f
Sebastian H. Gabrielli <sebgab@sebaweb.no>
2019-09-11 21:04:37 +0200

    No more canon seizure when getting close
    The canon no longer has a seizure when you get within 5 meters of it.
    It did introduce a small bug where you can shoot backwards tho.

--- Scripts/AimingAndShooting.cs
+++ Scripts/AimingAndShooting.cs
@@ -63,10 +63,11 @@ public class AimingAndShooting : MonoBehaviour
     }
 
     void setRotation () {
-      Quaternion rotation = getRotation();
-
-      // Set the rotation you just calculated to the objects rotation
-      transform.rotation = rotation;
+      Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      Vector3 lookAt = mouseScreenPosition;
+      float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
+      float AngleDeg = (180 / Mathf.PI) * AngleRad;
+      this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
     }
 
     void checkIfBulletExists () {
