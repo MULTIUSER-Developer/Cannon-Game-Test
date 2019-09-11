0ddfed792e0902b4006079c5c3b83e531c989e32
Sebastian H. Gabrielli <sebgab@sebaweb.no>
2019-09-11 16:46:08 +0200

    Fixed camera OOB movement
    Fixed the out of bounds movement of the camera that occured when you shoot

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -6,8 +6,8 @@ public class CameraController : MonoBehaviour
 {
   public Transform startPoint;
   public GameObject ObjectToFollow;
-  public float speed;
-  public float followSpeed;
+  public float speed = 100;
+  public float followSpeed = 100;
   [SerializeField]
   private bool BulletExists;
   [SerializeField]
@@ -33,9 +33,9 @@ public class CameraController : MonoBehaviour
 
     if (!BulletExists && waitTimeOver && transform.position != startPoint.position) {
       // Move the camera
-      transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
-    } else if (BulletExists) {
-      transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(ObjectToFollow.transform.position.x, transform.position.y, transform.position.z), followSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+    } else if (BulletExists && ObjectToFollow.transform.position.x >= startPoint.position.x) {
+      transform.position = Vector3.MoveTowards(transform.position, new Vector3(ObjectToFollow.transform.position.x, transform.position.y, transform.position.z), followSpeed * Time.deltaTime);
     }
   }
 }
